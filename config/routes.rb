5acd19162d2d0663bd2c37247f4c8b916d52c0e1
Wahid Kadri <wahidkadri.motiondesigner@gmail.com>
2018-05-15 16:31:12 +0200

    edit a task

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
   #  ALL
   get "tasks", to: "tasks#index"
   # CREATE
-  get "tasks/new", to: "tasks#new"
+  get "tasks/new", to: "tasks#new", as: :new
   post "tasks", to: "tasks#create"
   #  READ ONE
   get "tasks/:id", to: "tasks#show", as: :task
   # UPDATE
-  get "tasks/:id/edit", to: "tasks#edit"
-  patch "tasks/:id", to: "tasks#update"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update", as: :update
   # DESTROY
-  delete "tasks/:id", to: "tasks#destroy"
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy
 end
